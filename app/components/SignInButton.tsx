aea5f8dc60aeb73f51156a72649dc7bb32de128b
Alan Barbosa <alanzinhob@hotmail.com>
2024-04-18 14:49:52 -0300

    18_04

--- app/components/SignInButton.tsx
+++ app/components/SignInButton.tsx
@@ -1,11 +1,38 @@
+'use client'
 import Image from 'next/image'
 import React from 'react'
+import {signIn, signOut, useSession} from 'next-auth/react'
+
 
 export default function SignInButton() {
+
+    const {data:session} = useSession();
+
+
   return (
-    <>
-    <button type='button' className='text-white bg-blue-700 p-2 rounded-md hover:bg-blue-500'>Login</button>
-    <Image src="/next.svg" width={100} height={100} alt='Logo'></Image>
-    </>
+  <>
+            {session ? (
+                <>
+                    <button
+                        type='button'
+                        className='text-white bg-blue-700 py-2 px-4 rounded-md hover:bg-blue-500'
+                        onClick={() => signOut()}
+                    >
+                        Sair
+                    </button>
+                    <Image src="/next.svg" width={100} height={100} alt='Logo' />
+                </>
+            ) : (
+                <>
+                <button
+                        type='button'
+                        className='text-white bg-blue-700 py-2 px-4 rounded-md hover:bg-blue-500'
+                        onClick={() => signIn("google")}
+                    >
+                        Login
+                </button>
+                </>
+            )}
+        </>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import SignInButton from "./components/SignInButton";
+import Provider from './components/Provider';
 
 export default function Home() {
   return (
     <div className="flex w-full items-center justify-center gap-10 pt-10">
-
+      <Provider>
       <button>Home</button>
       <button>Server</button>
       <button>Client</button>
       <SignInButton/>
-
+      </Provider>
     </div>
   );
 }
