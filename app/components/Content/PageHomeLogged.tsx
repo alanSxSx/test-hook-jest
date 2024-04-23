b5d483f8c251b0dfed0eff353a84fe7104af4d56
Alan Barbosa <alanzinhob@hotmail.com>
2024-04-23 17:01:09 -0300

    23_04

---
+++ app/components/Content/PageHomeLogged.tsx
@@ -0,0 +1,25 @@
+'use client'
+import React, { useState } from 'react'
+import { useSession } from 'next-auth/react'
+
+export default function PageHomeLogged() {
+
+    const { data: session } = useSession();
+
+    const [exibirOcultar, setExibirOcultar] = useState(true);
+
+
+    function exibirOuOcultarNomeEmail() {
+        setExibirOcultar(!exibirOcultar)
+    }
+
+    return (
+        <div className='flex flex-col items-center justify-center gap-6 mt-10'>
+            <span className='text-2xl'>Seja Bem Vindo</span>
+            <span className='text-2xl'>Seu nome é: {exibirOcultar ? '' : <b>{session?.user?.name}</b>}</span>
+            <span>Seu e-mail cadastrado é: {exibirOcultar ? '' : <b>{session?.user?.email}</b>}</span>
+            <span>Clique no botão para ocultar seu nome e email:</span>
+            <button className='text-white bg-blue-700 py-2 px-4 rounded-md hover:bg-blue-500' onClick={exibirOuOcultarNomeEmail}>{exibirOcultar ? 'Exibir Mensagem' : 'Ocultar Mensagem'}</button>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,28 @@
 import Image from "next/image";
 import SignInButton from "./components/SignInButton";
 import Link from "next/link";
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]/route';
+import PageHomeLogged from "./components/Content/PageHomeLogged";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await getServerSession(authOptions);
+
+
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-12">
-      <Image src="/vercel.svg" className="flex items-center justify-center" width={300} height={300} alt='Logo' />
-      <p className="text-6xl flex text-center">Seja bem vindo.</p>
-    </div>
-  );
+    <>
+      {session ? (
+        <>
+         <PageHomeLogged/>
+        </>
+      ) : (
+        <div className="flex flex-col items-center justify-center min-h-screen gap-12">
+          <Image src="/vercel.svg" className="flex items-center justify-center" width={300} height={300} alt='Logo' />
+          <p className="text-6xl flex text-center">Seja bem vindo</p>
+        </div>
+      )}
+    </>
+  )
 }
